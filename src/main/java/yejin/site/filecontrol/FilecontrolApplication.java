5adc91052864ff1435b60951108f6e1b7d7cde9e
kyj2212 <kyj2212@gmail.com>
2022-09-05 14:26:03 +0900

    CHORE: init

---
+++ src/main/java/yejin/site/filecontrol/FilecontrolApplication.java
@@ -0,0 +1,13 @@
+package yejin.site.filecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilecontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yejin/site/filecontrol/FilecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package yejin.site.filecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
