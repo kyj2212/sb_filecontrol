797d49fc145d59b65d40f019c7cad4da9a2a9c5f
kyj2212 <kyj2212@gmail.com>
2022-09-15 14:50:03 +0900

    Fix: file:///

--- src/main/java/yejin/site/filecontrol/base/WebMvcConfig.java
+++ src/main/java/yejin/site/filecontrol/base/WebMvcConfig.java
@@ -1,18 +1,21 @@
 package yejin.site.filecontrol.base;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Slf4j
 public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${custom.genFileDirPath}")
     private String uploadDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.debug("파일 가져오기 시작 : uploadDir : "+ uploadDir);
         registry.addResourceHandler("/gen/**")
-                .addResourceLocations("file://"+uploadDir+"/");
+                .addResourceLocations("file:///"+uploadDir+"/");
     }
 }
