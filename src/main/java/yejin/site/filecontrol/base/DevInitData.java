4162ba5ff706a5d825c16128bea00faf291d6c1d
kyj2212 <kyj2212@gmail.com>
2022-09-15 15:26:49 +0900

    Feat: dev sample data and fix password encode once

--- src/main/java/yejin/site/filecontrol/base/DevInitData.java
+++ src/main/java/yejin/site/filecontrol/base/DevInitData.java
@@ -10,17 +10,16 @@ import yejin.site.filecontrol.base.exception.SignupUsernameDuplicatedException;
 import yejin.site.filecontrol.member.service.MemberService;
 
 @Configuration
-@Profile("test") // 이 클래스 정의된 Bean 들은 test 모드에서만 활성화 된다.
-public class TestInitData {
+@Profile("dev") // 이 클래스 정의된 Bean 들은 test 모드에서만 활성화 된다.
+public class DevInitData {
     // CommandLineRunner : 주로 앱 실행 직후 초기데이터 세팅 및 초기화에 사용
     @Bean
     CommandLineRunner init(MemberService memberService, PasswordEncoder passwordEncoder) {
         return args -> {
+            String password = passwordEncoder.encode("1234");
             try {
-                memberService.create("user1", "1234", "유저1","user1@test.com");
+                memberService.create("user1", "1234","유저1", "user1@test.com");
                 memberService.create("user2", "1234", "유저2","user2@test.com");
-                memberService.create("user3", "1234", "유저3","user3@test.com");
-                memberService.create("user4", "1234", "유저4","user4@test.com");
             } catch (SignupUsernameDuplicatedException e) {
                 throw new RuntimeException(e);
             } catch (SignupEmailDuplicatedException e) {
